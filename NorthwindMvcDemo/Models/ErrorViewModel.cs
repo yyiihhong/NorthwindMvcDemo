178fd650b1aa8b2b86e42f98855720a1033498c0
a0970 <a0970@LIBO>
2025-08-01 20:25:43 +0800

    加入專案檔案。

---
+++ NorthwindMvcDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NorthwindMvcDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
