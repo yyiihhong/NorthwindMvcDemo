ea9f5623f6cc28444156995db5a02fc441567491
YiHung <a0970861718@gmail.com>
2025-08-03 18:30:55 +0800

    feat:Scaffold建立Employees的控制器與檢視畫面

---
+++ NorthwindMvcDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NorthwindMvcDemo.Models;
+
+namespace NorthwindMvcDemo.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly northwindContext _context;
+
+        public EmployeesController(northwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var northwindContext = _context.Employees.Include(e => e.ReportsToNavigation);
+            return View(await northwindContext.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employees = await _context.Employees
+                .Include(e => e.ReportsToNavigation)
+                .FirstOrDefaultAsync(m => m.EmployeeID == id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+
+            return View(employees);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["ReportsTo"] = new SelectList(_context.Employees, "EmployeeID", "EmployeeID");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmployeeID,LastName,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath")] Employees employees)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employees);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ReportsTo"] = new SelectList(_context.Employees, "EmployeeID", "EmployeeID", employees.ReportsTo);
+            return View(employees);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employees = await _context.Employees.FindAsync(id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+            ViewData["ReportsTo"] = new SelectList(_context.Employees, "EmployeeID", "EmployeeID", employees.ReportsTo);
+            return View(employees);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmployeeID,LastName,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath")] Employees employees)
+        {
+            if (id != employees.EmployeeID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employees);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeesExists(employees.EmployeeID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ReportsTo"] = new SelectList(_context.Employees, "EmployeeID", "EmployeeID", employees.ReportsTo);
+            return View(employees);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employees = await _context.Employees
+                .Include(e => e.ReportsToNavigation)
+                .FirstOrDefaultAsync(m => m.EmployeeID == id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+
+            return View(employees);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employees = await _context.Employees.FindAsync(id);
+            if (employees != null)
+            {
+                _context.Employees.Remove(employees);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeesExists(int id)
+        {
+            return _context.Employees.Any(e => e.EmployeeID == id);
+        }
+    }
+}
